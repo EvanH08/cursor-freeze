e728956715e97c4ff966aa3aaeac7ca389833eab
EvanH08 <97171419+EvanH08@users.noreply.github.com>
2022-04-30 16:25:11 -0400

    Update and rename guess_the_number.cpp to cursorfreeze.cpp

--- cursorfreeze.cpp
+++ cursorfreeze.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <windows.h>
+#include <winuser.h>
 #include <conio.h>
 
 using namespace std;
 
 int main() 
 {
+    cout << "WARNING: This program may be harmful to your computer. Proceed? \n";
+    system("pause");
     for( ; ; ) 
     {
         SetCursorPos(0, 0);
